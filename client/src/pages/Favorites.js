6dfdd44dbc628912064d0daa257d0a4d2b5dadaa
Helena Heil <helenahcodes@gmail.com>
2021-07-02 18:16:38 +0200

    conditional rendering favourites

--- client/src/pages/Favorites.js
+++ client/src/pages/Favorites.js
@@ -1,4 +1,5 @@
 import PropTypes, { object } from 'prop-types';
+import styled from 'styled-components';
 
 import SightCard from '../components/SightCard';
 import { Headline } from './Sights';
@@ -7,16 +8,22 @@ export default function Favorites({ faveSights, toggleFavorite, isFave }) {
   return (
     <>
       <Headline>Favourites</Headline>
-      {faveSights.map((sight) => {
-        return (
-          <SightCard
-            key={sight._id}
-            sight={sight}
-            toggleFavorite={toggleFavorite}
-            isFave={isFave}
-          />
-        );
-      })}
+      {faveSights.length === 0 ? (
+        <ConditionalH4 style={{ textAlign: 'center' }}>
+          No favourites added yet ☘️
+        </ConditionalH4>
+      ) : (
+        faveSights.map((sight) => {
+          return (
+            <SightCard
+              key={sight._id}
+              sight={sight}
+              toggleFavorite={toggleFavorite}
+              isFave={isFave}
+            />
+          );
+        })
+      )}
     </>
   );
 }
@@ -26,3 +33,10 @@ Favorites.propTypes = {
   toggleFavorite: PropTypes.func,
   isFave: PropTypes.func,
 };
+
+const ConditionalH4 = styled.h4`
+  border-radius: 20px;
+  margin: 0.8rem 0;
+  padding: 0.5rem 1rem;
+  background-color: var(--grey-lightest-opa);
+`;
