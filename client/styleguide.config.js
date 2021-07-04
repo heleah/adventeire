491d2085b73428f7dc4d20a958b270e9f96617da
Helena Heil <helenahcodes@gmail.com>
2021-07-04 21:05:37 +0200

    add fonts to styleguidist

--- client/styleguide.config.js
+++ client/styleguide.config.js
@@ -4,4 +4,14 @@ module.exports = {
   styleguideComponents: {
     Wrapper: path.join(__dirname, 'src/StyleGuideWrapper'),
   },
+  template: {
+    head: {
+      links: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;800&family=Uncial+Antiqua&display=swap',
+        },
+      ],
+    },
+  },
 };
