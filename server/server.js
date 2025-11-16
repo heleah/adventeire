85b174ae550e946e2ba25b9839102e56a56e6491
Helena Heil <helenahcodes@gmail.com>
2025-11-16 16:29:18 +0100

    fix mongoose connect

--- server/server.js
+++ server/server.js
@@ -1,41 +1,40 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import path from 'path';
-import dirname from './lib/pathHelpers.js';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import path from "path";
+import dirname from "./lib/pathHelpers.js";
 
-import sightsRoutes from './routes/sights.routes.js';
+import sightsRoutes from "./routes/sights.routes.js";
 
 const __dirname = dirname(import.meta.url);
 
 dotenv.config();
 
 const connectionString =
-  process.env.DB_CONNECTION || 'mongodb://localhost:27017/adventeire';
+  process.env.DB_CONNECTION || "mongodb://localhost:27017/adventeire";
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false,
 });
 
-mongoose.set('returnOriginal', false);
+mongoose.set("returnOriginal", false);
 
 const server = express();
 
 server.use(cors());
 server.use(express.json());
 
-server.get('/health', (req, res) =>
-  res.json({ message: 'Server is running!' })
+server.get("/health", (req, res) =>
+  res.json({ message: "Server is running!" })
 );
 
 server.use(sightsRoutes);
 
-server.use(express.static(path.join(__dirname, '../client/build')));
-server.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+server.use(express.static(path.join(__dirname, "../client/build")));
+server.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 const port = process.env.PORT || 4000;
